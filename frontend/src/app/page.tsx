2dddeced1d6d2c9c3b8fcf66ecf2ee7a20dbe891
Rein Subisol <subisolreineerkim@gmail.com>
2025-09-06 18:07:20 +0800

    feat: base for the login done

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,12 +5,17 @@ import { SignIn, UserButton } from '@stackframe/stack'
 export default function Home() {
 
   return (
-    <div>
-      {/* If signed out → show SignIn */}
-      <SignIn/>
+    <div className='grid grid-cols-2 gap-4 p-4'>
+      <div className='flex flex-1 items-center justify-center border'>
+        <h1 className='text-4xl text-pink-400 font-bold'>Welcome to Albums!</h1>
+      </div>
+      <div className='flex flex-col items-center justify-center border'>
+        {/* If signed out → show SignIn */}
+        <SignIn />
 
-      {/* If signed in → show UserButton (avatar, logout, profile menu) */}
-      <UserButton />
+        {/* If signed in → show UserButton (avatar, logout, profile menu) */}
+        <UserButton />
+      </div>
     </div>
   );
 }
