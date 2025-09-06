65fcfcd60fad0563f1e967281be36d7dd11bf256
Rein Subisol <subisolreineerkim@gmail.com>
2025-09-06 18:09:48 +0800

    feat: setup the dependencies needed for designing

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
-import { SignIn, UserButton } from '@stackframe/stack'
+import { SignIn } from '@stackframe/stack'
+import { Button } from "@/components/ui/button"
 
 export default function Home() {
 
@@ -8,6 +9,7 @@ export default function Home() {
     <div className='grid grid-cols-2 gap-4 p-4'>
       <div className='flex flex-1 items-center justify-center border'>
         <h1 className='text-4xl text-pink-400 font-bold'>Welcome to Albums!</h1>
+        <Button className='bg-pink-400'>CLICK ME</Button>
       </div>
       <div className='flex flex-col items-center justify-center border'>
         {/* If signed out → show SignIn */}
