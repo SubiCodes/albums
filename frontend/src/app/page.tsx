fc9ac3ed46decfc318f30edbcff33e753b6db030
Rein Subisol <subisolreineerkim@gmail.com>
2025-09-08 18:21:50 +0800

    feat: edited the redirecting on the sign in page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -10,29 +10,32 @@ export default function Home() {
   const router = useRouter();
   const user = useUser();
 
-  const checkUser = async () => {
+  useEffect(() => {
     if (user) {
       router.push('/home');
     }
-  }
+  }, [user, router]);
 
-  useEffect(() => {
-    checkUser();
-  }, [user]);
+  if (user) {
+    return
+    <div className='min-w-screen min-h-screen flex items-center justify-center'>
+      Redirecting to your dashboard...
+    </div>;
+  }
 
   return (
     <div className='grid grid-cols-1 lg:grid-cols-2 gap-4 p-4 min-h-screen min-w-screen'>
       <div className='flex flex-1 flex-col items-center justify-center gap-12'>
         <h1 className='text-4xl text-pink-400 font-bold italic'>Welcome to Albums!</h1>
         <Image
-        src={'/login/login_image.png'}
-        alt='Login Image'
-        width={800}
-        height={800}
+          src={'/login/login_image.png'}
+          alt='Login Image'
+          width={800}
+          height={800}
         />
       </div>
       <div className='flex flex-col items-center justify-center '>
-         <div className="w-full max-w-lg flex items-center justify-center"> 
+        <div className="w-full max-w-lg flex items-center justify-center">
           <SignIn />
         </div>
       </div>
